a88857dc53a9b042aedb66bfc63b6972966ae0c8
faithchan <hannahfaith75@gmail.com>
2022-04-06 10:30:18 +0800

    added colors in tailwind config file

--- store-availability/tailwind.config.js
+++ store-availability/tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkgray: "#313132",
+        lightgray: "#f3f3f6",
+        green: "#3b9f50",
+        lighttext: "#9d9d9c",
+        mediumtext: "#706f6f",
+        silver: "#f0f1ec",
+        gold: "#f9e8d3",
+        alpinegreen: "#4d5d4c",
+        graphite: "#514f4d",
+        sierrablue: "#514f4d",
+        blueselector: "#0066cc",
+      },
+    },
   },
   plugins: [],
 };
