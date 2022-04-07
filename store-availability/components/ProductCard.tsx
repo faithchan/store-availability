1b228a9b691c46e27932af93bef8c3bcf3f89783
faithchan <hannahfaith75@gmail.com>
2022-04-07 22:52:27 +0800

    creating divider for the productcard

--- store-availability/components/ProductCard.tsx
+++ store-availability/components/ProductCard.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import GlobalContext from "../context/context";
 
 const ProductCard = () => {
-  return <div>Display iPhone Product Card</div>;
+  const { currentProduct } = useContext(GlobalContext);
+  return (
+    <div className="mx-20 mt-3">
+      <div className="bg-white w-full text-center  rounded-xl border">
+        {/* Title Section */}
+        <div className="grid grid-cols-1 my-4 divide-y">
+          <div className="text-3xl font-sf font-medium pb-4">
+            {currentProduct}
+          </div>
+          <div></div>
+        </div>
+        {/* End of Title Section */}
+
+        <div className="grid grid-cols-5 divide-x text-darkgray -mt-4">
+          <div className="text-3xl font-sf font-medium pb-4 col-span-2"></div>
+          <div className="col-span-3"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
