0634650e57e20a3b9365bb7656e371a2f5b89f83
faithchan <hannahfaith75@gmail.com>
2022-04-09 07:14:06 +0800

    fix index

--- store-availability/pages/index.tsx
+++ store-availability/pages/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import type { NextPage } from "next";
 
-const index = () => {
+const Home: NextPage = () => {
   return <div></div>;
 };
 
-export default index;
+export default Home;
